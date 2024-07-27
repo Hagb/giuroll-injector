1854a3d4de12774d1b84bf362f001ee2bd2fc431
Hagb (Junyu Guo) <hagb@hagb.name>
2024-07-27 08:45:08 +0800

    Free itself after loading giuroll

--- src/giuroll_loader_dll.rs
+++ src/giuroll_loader_dll.rs
@@ -1,8 +1,9 @@
 use std::ffi::CString;
+use std::time::Duration;
 use std::{os::windows::ffi::OsStringExt, path::Path, ptr::null_mut, thread};
 
-use winapi::shared::minwindef::FARPROC;
-use winapi::um::libloaderapi::FreeLibrary;
+use winapi::shared::minwindef::{FARPROC, HMODULE};
+use winapi::um::libloaderapi::{FreeLibrary, FreeLibraryAndExitThread};
 use winapi::{
     shared::minwindef::{DWORD, HINSTANCE, LPVOID},
     um::{
@@ -12,84 +13,72 @@ use winapi::{
         winuser::{MessageBoxA, MB_ICONERROR},
     },
 };
+unsafe fn try_load_giuroll(giuroll_path: Vec<u16>) -> Result<(), String> {
+    println!("loader thread sppawn");
+    let giuroll = LoadLibraryW(giuroll_path.as_ptr());
+    if giuroll == null_mut() {
+        return Err(format!(
+            "Failed to load giuroll.dll! Error code {}",
+            GetLastError()
+        ));
+    }
+    let initialize = match GetProcAddress(
+        giuroll,
+        CString::new("InitializeByLoader").unwrap().as_ptr() as *const i8,
+    ) as usize
+    {
+        0 => GetProcAddress(giuroll, CString::new("Initialize").unwrap().as_ptr()),
+        x => x as FARPROC,
+    };
+    if initialize == std::ptr::null_mut() {
+        return Err(format!(
+            "Failed to get InitializeByLoader or Initialize function of Giuroll. Error code {}",
+            GetLastError()
+        ));
+    }
+    let initialize = std::mem::transmute::<_, extern "C" fn(HINSTANCE) -> bool>(initialize);
+    if !initialize(giuroll) {
+        FreeLibrary(giuroll);
+        Err("Giuroll failed to initialize".to_string())
+    } else {
+        Ok(())
+    }
+}
 
 #[no_mangle]
 pub unsafe extern "stdcall" fn DllMain(module: HINSTANCE, reason: DWORD, _: LPVOID) -> i32 {
-    match reason {
-        DLL_PROCESS_ATTACH => {
-            println!("loader DllMain");
-            let mut dat = [0u16; 1025];
-            GetModuleFileNameW(module, dat.as_mut_ptr(), dat.len().try_into().unwrap());
-            let null_pos = dat.iter().position(|x| *x == 0).unwrap();
-            let path_ = std::ffi::OsString::from_wide(&dat[0..null_pos]);
-            let giuroll_path = Path::new(&path_).with_file_name("giuroll.dll");
-            if !giuroll_path.exists() {
-                eprintln!("{:?} not exists", giuroll_path);
-                return 0;
-            }
-            let giuroll_path = giuroll_path
-                .to_str()
-                .unwrap()
-                .encode_utf16()
-                .chain([0])
-                .collect::<Vec<u16>>();
-            thread::spawn(move || {
-                let title = CString::new("Failed to load Giuroll!").unwrap();
-                println!("loader thread sppawn");
-                let giuroll = LoadLibraryW(giuroll_path.as_ptr());
-                if giuroll == null_mut() {
-                    MessageBoxA(
-                        std::ptr::null_mut(),
-                        title.as_ptr(),
-                        CString::new(format!(
-                            "Failed to load giuroll.dll! Error code {}",
-                            GetLastError()
-                        ))
-                        .unwrap()
-                        .as_ptr(),
-                        MB_ICONERROR,
-                    );
-                    return;
-                }
-                let initialize = match GetProcAddress(
-                    giuroll,
-                    CString::new("InitializeByLoader").unwrap().as_ptr() as *const i8,
-                ) as usize
-                {
-                    0 => GetProcAddress(giuroll, CString::new("Initialize").unwrap().as_ptr()),
-                    x => x as FARPROC,
-                };
-                if initialize == std::ptr::null_mut() {
-                    MessageBoxA(
-                        std::ptr::null_mut(),
-                        title.as_ptr(),
-                        CString::new(format!(
-                            "Failed to get InitializeByLoader or Initialize function of Giuroll. Error code {}",
-                            GetLastError()
-                        ))
-                        .unwrap()
-                        .as_ptr(),
-                        MB_ICONERROR,
-                    );
-                    return;
-                }
-                let initialize =
-                    std::mem::transmute::<_, extern "C" fn(HINSTANCE) -> bool>(initialize);
-                if !initialize(giuroll) {
-                    MessageBoxA(
-                        std::ptr::null_mut(),
-                        title.as_ptr(),
-                        CString::new("Giuroll failed to initialize")
-                            .unwrap()
-                            .as_ptr(),
-                        MB_ICONERROR,
-                    );
-                    FreeLibrary(giuroll);
-                }
-            });
+    if reason == DLL_PROCESS_ATTACH {
+        println!("loader DllMain");
+        let mut dat = [0u16; 1025];
+        GetModuleFileNameW(module, dat.as_mut_ptr(), dat.len().try_into().unwrap());
+        let null_pos = dat.iter().position(|x| *x == 0).unwrap();
+        let path_ = std::ffi::OsString::from_wide(&dat[0..null_pos]);
+        let giuroll_path = Path::new(&path_).with_file_name("giuroll.dll");
+        if !giuroll_path.exists() {
+            eprintln!("{:?} not exists", giuroll_path);
+            return 0;
         }
-        _ => {}
+        let giuroll_path = giuroll_path
+            .to_str()
+            .unwrap()
+            .encode_utf16()
+            .chain([0])
+            .collect::<Vec<u16>>();
+        let module = module as usize;
+        thread::spawn(move || {
+            if let Err(e) = try_load_giuroll(giuroll_path) {
+                MessageBoxA(
+                    std::ptr::null_mut(),
+                    CString::new("Failed to load Giuroll!").unwrap().as_ptr(),
+                    CString::new(e).unwrap().as_ptr(),
+                    MB_ICONERROR,
+                );
+            }
+            thread::sleep(Duration::from_secs(3));
+            FreeLibraryAndExitThread(module as _, 0);
+        });
     }
+
     1
 }
 
